9294a08f8a7cf8209107adf4a2de949c6fd05289
postrv <71533421+postrv@users.noreply.github.com>
2024-01-24 15:12:34 +0000

    Adding in Main.rs

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+//mod add_nums_linked_list;
+//mod binary_search_tree;
+//mod btree_map;
+//mod dijkstra;
+//mod bubble_sort;
+//mod quick_sort;
+mod merge_sort;
+//mod zoeys_two_numbers;
+//mod employee_records;
+//mod hashmap;
+//mod heaps;
+//mod linked_lists;
+//mod pig_latin;
+//mod pretty_bst;
+//mod two_numbers;
+//mod two_nums_btree;
+
+fn main() {
+    //hashmap::main();
+    //pig_latin::main();
+    //employee_records::main();
+    //binary_search_tree::main();
+    //pretty_bst::main();
+    //btree_map::main();
+    //linked_lists::main();
+    //two_numbers::main();
+    //add_nums_links_list::main();
+    //heaps::main();
+    //dijkstra::main();
+    //bubble_sort::main();
+    merge_sort::main();
+}
